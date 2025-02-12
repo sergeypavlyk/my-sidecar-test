daed2043f0f69e0d49fe97fd31e8030b61167721
sergeypavlyk <pavlyk.sergeynik@gmail.com>
2025-02-12 17:05:53 +0200

    chore: initialize project with ESLint, Prettier, and TS configurations; add Tailwind CSS and Next.js setup

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,10 @@
+const LoginPage = async () => {
+   
+    return (
+        <section className="max-w-2xl w-full py-10">
+            Login
+        </section>
+    );
+};
+
+export default LoginPage;
